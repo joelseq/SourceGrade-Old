ae4c87b589c2ab7828e154e50ed14f29ae16b10d
Joel Sequeira <joelseq96@gmail.com>
2016-02-16 00:25:38 +0000

    First slightly working version of 2.0

--- app.js
+++ app.js
@@ -51,16 +51,79 @@ app.post("/scrape", function(req, res) {
           //Ridiculously specific selector to get the right a tags
           table.filter(function(){
                 var table = $(this);
+                //Get the tr containing the td we need
                 var tr = table.children().last();
+                //Get the td containing all the anchors we need
                 var td = tr.children().last();
+                //Find all the anchors
                 var anchors = td.find("a");
+                //For each anchor get the href
                 anchors.each(function(i, elem){
                    var ahref = $(this).attr("href");
                    var catUrl = baseUrl + ahref;
                    console.log(catUrl);
                    var categoryName = $(this).text();
                    var grade = new Object();
-                   grade = getGrade(id, catUrl);
+                   request(url, function(error,response,html){
+                    if(error){
+                        console.log(error);
+                    } else {
+                        var $ = cheerio.load(html);
+                        
+                        var rank; //user's rank
+                        var points; //user's points
+                        var score; //user's score (percentage)
+                        
+                        //Filter out the table containing the scores
+                        $('table').attr('cellpadding', '3').filter(function(){
+                            //Set this as table
+                            var table = $(this);
+                            //Get the first row of that table
+                            var row = table.children().first();
+                            //Boolean to check if ID was found
+                            var idFound = "false";
+                            //Loop through all the rows to find the row containing the ID
+                            for(var i = 0; i < table.children().length; i++){
+                                if(row.children().first().text() === id){
+                                    console.log("ID found");
+                                    $('row').children().attr('bgcolor', '#FFFFD0').filter(function(){
+                                        //Get the NodeList of the children
+                                        var tds = $(this);
+                                        var td = tds.children().first();
+                                        if(tds.length === 3){
+                                            console.log("If statement entered");
+                                            rank = td.text();
+                                            td = td.next();
+                                            points = td.text();
+                                            td = td.next();
+                                            score = td.text();
+                                            idFound = "true";
+                                            var returnVar = {ra: rank, po: points, sc: score};
+                                            grade = returnVar;
+                                            return;
+                                        } else {
+                                            console.log("Else statement entered");
+                                            rank = td.text();
+                                            td = td.next();
+                                            score = td.text();
+                                            var returnVar = {ra: rank, sc: score};
+                                            grade = returnVar;
+                                            return;
+                                        }
+                                    }); /* end of filter */
+                                } else if(row.next() !== undefined) {
+                                    row = row.next();
+                                } else {
+                                    console.log("id not found");   
+                                }
+                            } /* end of for loop */
+                        }); /* end of filter */
+                    }
+                }); /* end of request */
+    
+    
+    
+    
                    console.log(grade);
                    if(grade.length === 3) {
                     toReturn = "For " + categoryName + " you got rank " + grade.ra + 
@@ -83,79 +146,7 @@ app.post("/scrape", function(req, res) {
 
 
 
-
-/* ============================================================================
-    function name: getGrade
-    parameters: id -- the user's id
-                url -- the url of the link to get the information from
-    return: object containing the rank, points(optional), and score.
-   ============================================================================ */
-function getGrade(id, url) {
-    console.log("Entered getGrade function");
-    request(url, function(error,response,html){
-        console.log("Entered request");
-        if(error){
-            console.log("Reached error");
-            console.log(error);
-        } else {
-            console.log("reached here atleast!");
-            var $ = cheerio.load(html);
-            
-            var rank; //user's rank
-            var points; //user's points
-            var score; //user's score (percentage)
-            
-            //Filter out the table containing the scores
-            $('table').attr('cellpadding', '3').filter(function(){
-                console.log("reached this far!");
-                //Set this as table
-                var table = $(this);
-                //Get the first row of that table
-                var row = table.children().first();
-                //Boolean to check if ID was found
-                var idFound = "false";
-                //Loop through all the rows to find the row containing the ID
-                for(var i = 0; i < table.children().length; i++){
-                    if(row.children().first().text() === id){
-                        console.log("ID found");
-                        $('row').children().attr('bgcolor', '#FFFFD0').filter(function(){
-                            //Get the NodeList of the children
-                            var tds = $(this);
-                            var td = tds.children().first();
-                            if(tds.length === 3){
-                                console.log("If statement entered");
-                                rank = td.text();
-                                td = td.next();
-                                points = td.text();
-                                td = td.next();
-                                score = td.text();
-                                idFound = "true";
-                                var returnVar = {ra: rank, po: points, sc: score};
-                                console.log(returnVar);
-                                return returnVar;
-                            } else {
-                                console.log("Else statement entered");
-                                rank = td.text();
-                                td = td.next();
-                                score = td.text();
-                                var returnVar = {ra: rank, sc: score};
-                                console.log(returnVar);
-                                return returnVar;
-                            }
-                        }); /* end of filter */
-                    } else if(row.next() !== undefined) {
-                        row = row.next();
-                    } else {
-                        console.log("id not found");   
-                    }
-                } /* end of for loop */
-            }); /* end of filter */
-        }
-    }); /* end of request */
-} /* end of function */
-
-
-
+/* Function to get the base url */
 function replaceUrl(i, url) {
     var str = url.substr(url.lastIndexOf('/') + 1) + '$';
     return url.replace( new RegExp(str), '' );
