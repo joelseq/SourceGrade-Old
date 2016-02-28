6f0e04cee3a3155079e4e3c97a5eac6e537c2463
Joel Sequeira <joelseq96@gmail.com>
2016-02-28 04:19:40 +0000

    SourceGrade working v2.0

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 //Set the view engine to ejs
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + "/public"));
+
 
 
 /* ====================================================
@@ -38,121 +40,120 @@ app.post("/scrape", function(req, res) {
       if(error) {
           console.log(error);
       } else {
-          var $ = cheerio.load(html);
+          if(!url.endsWith('index.html')){
+              res.render("error");
+          } else {
+              var $ = cheerio.load(html);
           
-          var baseUrl = replaceUrl(1, url); //The 1 is just for the paramater
-          
-          var tables = $('center>table');
-          
-          var table;
-          
-          tables.each(function() {
-              if($(this).attr("width", "50%"))
-                table = $(this);
-          });
-          
-          //Filter out the table to reach the anchor tags
-          table.filter(function(){
-                var table = $(this);
-                //Get the tr containing the td we need
-                var tr = table.children().last();
-                //Get the td containing all the anchors we need
-                var td = tr.children().last();
-                //Find all the anchors
-                var anchors = td.find("a");
-                //For each anchor get the href
-                anchors.each(function(i, elem){
-                   var ahref = $(this).attr("href");
-                   var catUrl = baseUrl + ahref;
-                   //Create grade object for that specific anchor
-                   var grade = {
-                       name: $(this).text(),
-                       url: catUrl,
-                       rank: "",
-                       points: "",
-                       score: ""
-                   };
-                   grades.push(grade);
-                }); /* end of for each */
-            
-          }); /* end of filter */
-          
-          //Make an asynchronous request for each url
-          async.each(grades, function(grade, done){
-              request(grade.url, function(error, response, html){
-                    if(error){
-                        console.log(error);
-                    } else {
-                        var $ = cheerio.load(html);
-                        
-                        //Filter out the table containing the scores
-                        $('table').attr('cellpadding', '3').filter(function(){
-                            //Set this as table
-                            var table = $(this);
-                            //Get the first row of that table
-                            var row = table.children().first();
-                            //Loop through all the rows to find the row containing the ID
-                            for(var i = 0; i < table.children().length; i++){
-                                if(row.children().first().text() === id){
-                                    var items = [];
-                                    var child = row.children().first();
-                                    for(var j = 0; j < row.children().length; j++) {
-                                        if(child.attr('bgcolor','#FFFFD0') == true) {
-                                            items.push(child.text());
-                                            console.log("Item being pushed is " + child.text());
+              var baseUrl = replaceUrl(1, url); //The 1 is just for the paramater
+              
+              var tables = $('center>table');
+              
+              var table;
+              
+              tables.each(function() {
+                  if($(this).attr("width", "50%"))
+                    table = $(this);
+              });
+              
+              //Filter out the table to reach the anchor tags
+              table.filter(function(){
+                    var table = $(this);
+                    //Get the tr containing the td we need
+                    var tr = table.children().last();
+                    //Get the td containing all the anchors we need
+                    var td = tr.children().last();
+                    //Find all the anchors
+                    var anchors = td.find("a");
+                    //For each anchor get the href
+                    anchors.each(function(i, elem){
+                       var ahref = $(this).attr("href");
+                       var catUrl = baseUrl + ahref;
+                       //Create grade object for that specific anchor
+                       var grade = {
+                           name: $(this).text(),
+                           url: catUrl,
+                           rank: "",
+                           points: "",
+                           score: ""
+                       };
+                       grades.push(grade);
+                    }); /* end of for each */
+                
+              }); /* end of filter */
+              
+              //Make an asynchronous request for each url
+              async.each(grades, function(grade, done){
+                  request(grade.url, function(error, response, html){
+                        if(error){
+                            res.render("error");
+                            console.log(error);
+                        } else {
+                            var $ = cheerio.load(html);
+                            
+                            //Filter out the table containing the scores
+                            $('table').attr('cellpadding', '3').filter(function(){
+                                //Set this as table
+                                var table = $(this);
+                                //Get the first row of that table
+                                var row = table.children().first();
+                                //Loop through all the rows to find the row containing the ID
+                                for(var i = 0; i < table.children().length; i++){
+                                    if(row.children().first().text() === id){
+                                        var index;
+                                        var items = [];
+                                        var child = row.children().first();
+                                        //Loop through the children of the row
+                                        //to find the highlighted ones
+                                        for(var j = 0; j < row.children().length; j++) {
+                                            if(child.attr('bgcolor') === '#FFFFD0') {
+                                                index = j;
+                                                items.push(child.text());
+                                            }
+                                            child = child.next();
                                         }
-                                        child = child.next();
-                                    }
-                                    if(items.length === 3) {
-                                        grade.rank = items[0];
-                                        grade.points = items[1];
-                                        grade.score = items[2];
+                                        //If 3 items, there is rank, points, and score
+                                        if(items.length === 3) {
+                                            grade.rank = items[0];
+                                            grade.points = items[1];
+                                            grade.score = items[2];
+                                            //Get to the 3rd row
+                                            var mainRow = table.children().eq(2);
+                                            var pointsColumn = mainRow.children().eq(index-1);
+                                            grade.points+=" / " + pointsColumn.text();
+                                            var scoreColumn = mainRow.children().eq(index);
+                                            grade.score+="/" + scoreColumn.text();
+                                        } else {
+                                            grade.rank = items[0];
+                                            grade.score = items[1];
+                                            var mainRow = table.children().eq(2);
+                                            var scoreColumn = mainRow.children().eq(index);
+                                            grade.score+= " / " + scoreColumn.text();
+                                        }
+                                        //Exit out of loop
+                                        break;
+                                        
                                     } else {
-                                        grade.rank = items[0];
-                                        grade.score = items[1];
+                                        row = row.next();
                                     }
-                                    // var main = row.children().first();
-                                    // //Get the NodeList of the children
-                                    // main.siblings().filter(function(){
-                                    //     var tds = $(this).attr('bgcolor','#FFFFD0');
-                                    //     //If 3 then one td is for points
-                                    //     if(tds.length === 3){
-                                    //         var td = tds.first();
-                                    //         grade.rank = td.text();
-                                    //         td = td.next();
-                                    //         grade.points = td.text();
-                                    //         td = td.next();
-                                    //         grade.score = td.text();
-                                    //     } else { //Else only rank and score
-                                    //         var td = tds.first();
-                                    //         grade.rank = td.text();
-                                    //         td = td.next();
-                                    //         grade.score = td.text();
-                                    //     }
-                                    // });
-                                    //Exit out of loop
-                                    break;
-                                    
-                                } else {
-                                    row = row.next();
-                                }
-                            } /* end of for loop */
+                                } /* end of for loop */
+                                
+                                
+                            }); /* end of filter */
                             
                             
-                        }); /* end of filter */
-                        
-                        
-                    } /* end of else */
-                 done(); 
-              }); /* end of request */
-          }, function(err){
-              if(err){
-                  console.log(err);
-              } else {
-                  console.log("Done parsing grades");
-                  res.render("scrape",{grades: grades});
-              }
-          });
+                        } /* end of else */
+                     done(); 
+                  }); /* end of request */
+              }, function(err){
+                  if(err){
+                      console.log(err);
+                  } else {
+                      console.log("Done parsing grades");
+                      res.render("scrape",{grades: grades});
+                  }
+              });
+          }
           
       } /* end of gigantic else */
       
