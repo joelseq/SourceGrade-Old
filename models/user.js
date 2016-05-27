3a19c54aa316f8399e5e1ad7314e0c0658eda038
Joel Sequeira <joelseq96@gmail.com>
2016-05-26 18:53:15 -0700

    Add node_modules to make heroku work

--- models/user.js
+++ models/user.js
@@ -5,7 +5,10 @@ const mongoose = require('mongoose'),
 const UserSchema = new mongoose.Schema({
   username: String,
   password: String,
-  classes: [Class.classSchema]
+  classes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Class"
+  }]
 });
 
 UserSchema.plugin(passportLocalMongoose);
